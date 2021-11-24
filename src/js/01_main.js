baab5fe57880583fb48fba4c553e3c1d42e0aed1
Desginer123 <hotsru12@gmail.com>
2021-11-25 06:08:28 +0800

    main page ready, price start 01:10

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,7 +1,7 @@
 let burger = document.querySelector('.hamburger'),
     tabletMenu = document.querySelector('.tablet-header__menu'),
     dropdownLinks = document.querySelectorAll('.tablet-header__menu .nav__item.dropdown'),
-    dropdownContents = document.querySelectorAll('.dropdown-content');
+    dropdownContents = document.querySelectorAll('.dropdown-content-tablet');
 
 burger.addEventListener('click', () => {
   tabletMenu.classList.toggle('hidden');
@@ -24,3 +24,17 @@ dropdownLinks.forEach((link, i) => {
   })
 
 });
+
+if(window.innerWidth < 766) {
+  let stepImage = document.querySelectorAll('.steps-items__img');
+  let stepWebp = document.querySelectorAll('.steps-items__item picture source');
+
+  console.log(stepImage[0])
+  stepImage[0].setAttribute('src', './img/step-1-m.png')
+  stepWebp[0].setAttribute('srcset', './img/step-1-m.webp')
+  stepImage[1].setAttribute('src', './img/step-2-m.png')
+  stepWebp[1].setAttribute('srcset', './img/step-2-m.webp')
+  stepImage[2].setAttribute('src', './img/step-3-m.png')
+  stepWebp[2].setAttribute('srcset', './img/step-3-m.webp')
+
+}
